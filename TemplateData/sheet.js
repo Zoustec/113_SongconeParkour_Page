b5685c0dcc2445f8c8c7223fca0a6ecfa5610c91
Ryan-zoustec <Ryan@zoustec.com>
2024-10-08 18:26:52 +0800

    雙績分表用https://......?group=0做切換

--- TemplateData/sheet.js
+++ TemplateData/sheet.js
@@ -1,4 +1,17 @@
-const sheetUrl = "https://script.google.com/macros/s/AKfycbyjSQnWJhwuWSRZYxd8jOtkOwF99ZNSUBp8bDGyNYlcIAD_II6hAoK7elOdQRWz6Y7dkw/exec";
+// 獲取當前 URL 的查詢參數
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const group = urlParams.get('group');
+
+// 根據 'group' 的值設定重定向網址
+let sheetUrl = "";
+if (group === '0') {
+    //兒童組績分
+    sheetUrl = "https://script.google.com/macros/s/AKfycbypgPpW-ZYZWczF2yfYUzqW5UFVexZjSijN1Azdq8fO1c1OjgzPMqk6Jw-Ct7OftW-ffg/exec";
+} else {
+    //成人組績分
+    sheetUrl = "https://script.google.com/macros/s/AKfycbyjSQnWJhwuWSRZYxd8jOtkOwF99ZNSUBp8bDGyNYlcIAD_II6hAoK7elOdQRWz6Y7dkw/exec";
+}
 
 const sheet = {
     Send: function() {
