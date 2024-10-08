e278344ba1edc4dfff6ec8bc1563884e48d2b47a
Ryan-zoustec <Ryan@zoustec.com>
2024-10-08 20:28:17 +0800

    Update 記錄localStorage

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,7 +1,7 @@
 const cacheName = "Zoustec ltd.-SongconeParkour-1.0.3";
 const contentToCache = [
     "Build/1.0.3.loader.js",
-    "Build/1.0.3.framework.js",
+    "Build/1.0.3.framework.js.unityweb",
     "Build/1.0.3.data.unityweb",
     "Build/1.0.3.wasm.unityweb",
     "TemplateData/style.css"
@@ -9,42 +9,39 @@ const contentToCache = [
 ];
 
 self.addEventListener('install', function (e) {
-  console.log('[Service Worker] Install');
-  
-  e.waitUntil((async function () {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: app shell and content');
       await cache.addAll(contentToCache);
-  })());
+    })());
 });
 
 self.addEventListener('fetch', function (e) {
-  e.respondWith((async function () {
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      
-      // 先從快取中匹配請求
-      let response = await caches.match(e.request);
-      
-      if (response) {
-          console.log(`[Service Worker] Found in cache: ${e.request.url}`);
-          return response; // 如果找到了，直接返回快取的響應
-      }
+    e.respondWith((async function () {
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        
+        let response = await caches.match(e.request);
+        
+        if (response) {
+            console.log(`[Service Worker] Found in cache: ${e.request.url}`);
+            return response;
+        }
 
-      // 如果快取中找不到，發起網絡請求
-      try {
-          response = await fetch(e.request);
+        try {
+            response = await fetch(e.request);
 
-          // 只對 GET 請求進行快取
-          if (e.request.method === 'GET') {
-              const cache = await caches.open(cacheName);
-              console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-              cache.put(e.request, response.clone()); // 快取響應
-          }
+            if (e.request.method === 'GET') {
+                const cache = await caches.open(cacheName);
+                console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+                cache.put(e.request, response.clone());
+            }
 
-          return response; // 返回網絡響應
-      } catch (error) {
-          console.error(`[Service Worker] Fetch failed: ${error}`);
-          throw error; // 錯誤處理
-      }
-  })());
+            return response;
+        } catch (error) {
+            console.error(`[Service Worker] Fetch failed: ${error}`);
+            throw error;
+        }
+    })());
 });
